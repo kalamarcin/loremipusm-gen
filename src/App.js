32e6be574931eb6fd4e633fc963554dc695053cd
Marcin Kala <kalamarcin1@gmail.com>
2022-10-24 12:30:23 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react'
+import data from './data'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount] = useState(0)
+	const [text, setText] = useState([])
+
+	const handleSubmit = e => {
+		e.preventDefault()
+		setText(data)
+	}
+	return (
+		<>
+			<section className="section-center">
+				<h3>Zmączony nudnym lorem ipsum?</h3>
+				<form className="lorem-form" onSubmit={handleSubmit}>
+          <label htmlFor="amout">
+            Paragrafy:
+          </label>
+          <input type="number" name='amout' id='amount' value={count} onChange={(e)=>setCount(e.target.value)}/>
+          <button type='submit' className='btn'>generuj</button>
+        </form>
+        <article className='lorem-text'>
+          {text.map((item, index) => {
+            return (
+              <p key={index}>{item}</p>
+            )
+          })}
+        </article>
+			</section>
+		</>
+	)
 }
 
-export default App;
+export default App
